016e281cbe4afb91e3d620b105edad2b3261fab0
Rubayet Hasan <rubayet09csejust@gmail.com>
2021-01-15 17:28:41 +0100

    initiate py

---
+++ initiate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import subprocess
+import string
+
+subprocess.run("scrapy crawl euroJobsCountryWiseJobPostingCount".split())
+
+# In[ ]:
+
+
+
+
