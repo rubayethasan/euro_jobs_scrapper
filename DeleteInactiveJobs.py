dc398053401967f3c4d9ceabe045788a189b9a41
Rubayet Hasan <rubayet09csejust@gmail.com>
2021-04-28 11:52:16 +0200

    comment

--- DeleteInactiveJobs.py
+++ DeleteInactiveJobs.py
@@ -14,12 +14,14 @@ class DeleteInactiveJobs:
         self.connection.row_factory = sqlite.Row
         self.cursor = self.connection.cursor()
     
+    '''Method for checking a job posting active or not'''
     def isActive(self,url):
         if requests.get(url).status_code == 200:
             return True
         else:
             return False
         
+    '''Method for deleteing job posting form database tables'''  
     def deleteFromDb(self,url):
         for table in self.tables:
             qry = "DELETE FROM " + table + " WHERE url = '" + url + "'"
@@ -27,18 +29,21 @@ class DeleteInactiveJobs:
             self.connection.commit()
             print('Inactive URL:',url,'Deleted From',table)
 
+    '''Method for deleteing single job posting if inactive'''        
     def deleteOneInactive(self,url,delete_from_tables):
         self.tables = delete_from_tables
         if not self.isActive(url):
             self.deleteFromDb(url)
         else:
             print('Active URL:',url)
-            
+           
+    '''Method for deleteing multiple job posting if inactive'''   
     def deleteAllIncative(self,check_table,delete_from_tables):
         self.cursor.execute('SELECT * FROM '+check_table)
         for row in self.cursor.fetchall():
             self.deleteOneInactive(row['url'],delete_from_tables)
 
+'''Method for deleteing inactive jobs'''   
 def delete_inactive_jobs():
     DeleteInactiveJobs().deleteAllIncative('jobdetails',['joburls','jobdetails'])
 
